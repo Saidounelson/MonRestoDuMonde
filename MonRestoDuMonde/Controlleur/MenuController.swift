0cd04299a185f2c35367cd17a9c73ae912342ed9
Saidou N <nasmadiba@gmail.com>
2018-06-18 15:36:56 +0000

    Ajout des controlleurs principales

--- MonRestoDuMonde/Controlleur/MenuController.swift
+++ MonRestoDuMonde/Controlleur/MenuController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MenuController.swift
 //  MonRestoDuMonde
 //
 //  Created by mac on 18/06/2018.
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MenuController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+     
+    }
+    
+
+   
 
 }
-
